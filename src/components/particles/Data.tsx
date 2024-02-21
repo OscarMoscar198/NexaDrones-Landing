c6bd36375126e7001ed34da7bfcd1478e9b2c789
Oscar Castañeda <109644116+OscarMoscar198@users.noreply.github.com>
2024-02-21 13:23:24 -0600

    Fist commit

---
+++ src/components/particles/Data.tsx
@@ -0,0 +1,124 @@
+export const NavLinks = [
+    {
+        name: "Inicio",
+        url: "/"
+    },
+    {
+        name: "Sobre Nosotros",
+        url: "/about"
+    },
+    {
+        name: "Contacto",
+        url: "/contact"
+    },
+];
+
+export const HeroTexts = [
+    {
+        Heading: "Siente la Adrenalina con Nuestros Drones de Alta Velocidad",
+        Paragraph: "¡Impulsa la velocidad y alcanza la grandeza con nuestros drones de alta velocidad! Únete a nuestra comunidad de apasionados por la tecnología y emprende un viaje emocionante hacia el futuro de la exploración aérea.",
+        Button: "Únete Hoy"
+    },
+    {
+        Heading: "Desafía los Cielos con NexaDrones",
+        Paragraph: "¡Bienvenido a NexaDrones, donde la innovación se encuentra con la excelencia! Descubre un mundo de posibilidades con nuestros drones de carga diseñados para llevar tus cargas más pesadas a nuevas alturas.",
+        Button: "Explora Ahora"
+    },
+    {
+        Heading: "Captura Momentos Épicos con NexaDrones",
+        Paragraph: "En NexaDrones, llevamos la grabación a nuevas alturas con nuestros drones de grabación de última generación. Únete a nosotros en esta emocionante aventura donde la diversión y los resultados van de la mano.",
+        Button: "Descubre Más"
+    }
+]
+
+
+export const AboutTexts = {
+    firstText: "Sobre NexaDrones",
+    secondText: "Bienvenido",
+    caption: "Bienvenido a NexaDrones, donde la innovación se encuentra con la excelencia y la transformación se hace realidad.",
+    paragraph1: "En NexaDrones, estamos comprometidos con la excelencia y la innovación en el mundo de los drones. Nuestra misión es proporcionar soluciones avanzadas y fiables que impulsen el progreso y la eficiencia en diversas industrias."
+}
+
+export const OfferTexts = {
+    firstText: "Lo que Ofrecemos",
+    secondText: "Nuestros Servicios",
+    list: [
+        {
+            listCaption: "Exploración Aérea",
+            text: "Ofrecemos drones de última generación diseñados para explorar y mapear áreas de difícil acceso, proporcionando datos precisos y útiles para una variedad de aplicaciones industriales y comerciales.",
+        },
+        {
+            listCaption: "Fotografía y Videografía Aérea",
+            text: "Nuestros drones están equipados con cámaras de alta resolución que capturan imágenes y videos impresionantes desde perspectivas únicas, ideales para producción audiovisual, inspecciones y mucho más.",
+        },
+        {
+            listCaption: "Entrega y Logística",
+            text: "Implementamos soluciones de entrega y logística utilizando drones, optimizando la eficiencia y reduciendo los costos en una variedad de sectores, desde el comercio electrónico hasta la atención médica.",
+        }
+    ],
+}
+
+export const ContactTexts = {
+    firstText: "Contáctanos Hoy",
+    phone: "(+52) 424-248-8496",
+    paragraph: "En NexaDrones, estamos comprometidos con proporcionarte soluciones aéreas avanzadas y un servicio al cliente excepcional. No dudes en contactarnos para discutir tus necesidades y descubrir cómo podemos ayudarte a alcanzar tus objetivos.",
+    button: "Programar una Reunión"
+}
+
+export const TestimonialTexts = {
+    firstText: "Testimonios",
+    secondText: "Lo Que Dicen Nuestros Clientes",
+    feedBacks: [
+        {
+            text: "Los drones de NexaDrones han transformado nuestra forma de recopilar datos y realizar inspecciones. Son confiables, precisos y fáciles de operar. ¡Altamente recomendados!",
+            person: "Mercado Libre",
+            type: "Cliente"
+        },
+        {
+            text: "La fotografía aérea proporcionada por NexaDrones ha elevado el nivel de nuestras producciones audiovisuales. ¡Los resultados son simplemente impresionantes!",
+            person: "Amazon",
+            type: "Cliente"
+        },
+        {
+            text: "Gracias a la logística de entrega de NexaDrones, hemos optimizado nuestros procesos y reducido los costos operativos. ¡Una solución innovadora que realmente funciona!",
+            person: "Domino's Pizza",
+            type: "Cliente"
+        }
+    ],
+}
+
+export const FooterTexts = {
+    underLogoText: "En NexaDrones, nos enorgullece ofrecer soluciones aéreas avanzadas y un servicio al cliente excepcional. Nuestra pasión por la innovación y la excelencia impulsa todo lo que hacemos.",
+    quickLinks: {
+        caption: "Enlaces Rápidos",
+        links: [
+            {
+                name: "Inicio",
+                url: "/"
+            },
+            {
+                name: "Sobre Nosotros",
+                url: "/about"
+            },
+            {
+                name: "Contacto",
+                url: "/contact"
+            },
+        ]
+    },
+    contacts: {
+        caption: "Contacto Rápido",
+        names: [
+            {
+                name: "11567 Santa Monica Blvd, Los Angeles, CA 90025, Estados Unidos",
+            },
+            {
+                name: "NexaDrones@gmail.com",
+            },
+            {
+                name: "(+52) 424-248-8496",
+            }
+        ]
+    },
+    copyright: "Derechos de Autor 2024. Todos los Derechos Reservados"
+}
